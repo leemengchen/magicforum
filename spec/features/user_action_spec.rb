ade887116fa05fa3f5884fb9956cab9e68a9ce7f
leemengchen <orange_2039@hotmail.com>
2016-08-25 19:53:14 +0800

    finish model testing and partially on feature testing

--- spec/features/user_action_spec.rb
+++ spec/features/user_action_spec.rb
@@ -2,10 +2,11 @@ require "rails_helper"
 
 RSpec.feature "User Navigation", type: :feature ,js: true do
   before(:all) do
-    @user = create(:user)
+    @user = create(:user, :sequenced_email, :sequenced_username)
     @admin = create(:user, :admin)
     @topic = create(:topic)
-    # @topic = create(:topic)
+    @post = create(:post,topic_id:@topic.id, user_id: @user.id)
+
   end
 
   scenario "admin log in and create and  topic" do
@@ -61,7 +62,7 @@ RSpec.feature "User Navigation", type: :feature ,js: true do
         expect(topic.title).to eql("Topic123")
   end
 
-  scenario "user create a topic" do
+  scenario "user create a Post" do
 
 
         visit root_path
@@ -94,6 +95,41 @@ RSpec.feature "User Navigation", type: :feature ,js: true do
         post = Post.find_by(title:"Post title 123456")
 
         expect(post.title).to eql("Post title 123456")
+  end
+
+  scenario "user create a Comment" do
+
+
+        visit root_path
+
+        click_button('LOGIN')
+
+        fill_in 'user_email_field', with: @user.email
+        fill_in 'user_password_field', with: @user.password
+
+        click_button('Login')
+
+        visit topic_post_comments_path(@topic, @post)
+
+        fill_in 'comment_body_field', with: "Comment body 123"
+
+        click_button("Create Comment")
+
+        expect(find('.flash-messages .message').text).to eql("You've created a new comment.")
+
+        visit topic_post_comments_path(@topic,@post)
+
+        click_link("Edit Comment")
+
+        fill_in  "comment_body_field", with:"Comment body 123456"
+
+        click_button("Update Comment")
+
+        comment = Comment.find_by(body:"Comment body 123456")
+
+        expect(find('.flash-messages .message').text).to eql("You're successfullly update your comment!")
+        expect(comment.body).to eql("Comment body 123456")
+        expect(page).to have_content("Comment body 123456")
   end
 
 
